b146773215052466d1704ded29103bfa6c900b4b
dharanchilukuri <dharanchilukuri@gmail.com>
2024-10-10 08:47:50 +0530

    updated css

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,11 +3,25 @@ import React from "react";
 function TaskList({ tasks, toggleComplete, deleteTask, editTask }) {
   return (
     <ul>
+        {tasks.length > 0 && (
+        <p className="info-text">Click anywhere on the text to mark it as completed</p>
+        )}  
       {tasks.map((t, index) => (
-        <li key={index} className={t.completed ? "completed" : ""}>
-          <span onClick={() => toggleComplete(index)}>{t.text}</span>
-          <button onClick={() => editTask(index)}>Edit</button>
-          <button onClick={() => deleteTask(index)}>Delete</button>
+         <li
+         key={index}
+         onClick={() => toggleComplete(index)}
+         className={t.completed ? "completed" : ""}
+         style={{ cursor: "pointer" }}
+       >
+         <span className="task-text">{t.text}</span>
+         <button onClick={(e) => {
+            e.stopPropagation(); 
+            editTask(index);
+          }}>Edit</button>
+          <button onClick={(e) => {
+            e.stopPropagation(); 
+            deleteTask(index);
+          }}>Delete</button>
         </li>
       ))}
     </ul>
